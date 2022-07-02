943a9be947cd54c03f343d071ac2e9c3ea99bbec
Mohammad Razi ul Haq <razi.ulhaq@creditbook.pk>
2022-07-02 11:01:58 +0500

    Initial commit

---
+++ scripts/excel_to_csv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+excel_file = '/Users/raziulhaq/Documents/Work/cascade/skills-assessment-data-engineer/carmen_sightings_20220629061307.xlsx'
+all_sheets = pd.read_excel(excel_file, sheet_name=None)
+sheets = all_sheets.keys()
+
+for sheet_name in sheets:
+    sheet = pd.read_excel(excel_file, sheet_name=sheet_name)
+
+    ## removing specialcharacter/non-english characters
+    sheet.columns=sheet.columns.str.replace(r'[^0-9a-zA-Z ]', '', regex=True)
+    sheet.to_csv("/Users/raziulhaq/Documents/Work/cascade/case_study/data/%s.csv" % sheet_name, index=False)
+print("Wrote all sheets as csv")
